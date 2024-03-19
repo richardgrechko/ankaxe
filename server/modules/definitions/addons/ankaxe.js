6f0bf04033fa8e04cdf58a578462ee18e556418a
richardgrechko <162537734+richardgrechko@users.noreply.github.com>
2024-03-19 08:27:17 +0500

    Update ankaxe.js

--- server/modules/definitions/addons/ankaxe.js
+++ server/modules/definitions/addons/ankaxe.js
@@ -1,20 +1,76 @@
 const { combineStats, addAura, makeAuto } = require('../facilitators.js');
 const { gunCalcNames, smshskl, base } = require('../constants.js');
 const g = require('../gunvals.js');
-module.exports = ({ Class, config }) => {
-  // Weapons
-    exports.node = {
+module.exports = ({ Class, Config }) => {
+  Config.SPAWN_CLASS = ["ankaxe_node", "ankaxe_base"]
+  
+    Class.ankaxe_circle = {
         PARENT: ["genericTank"],
-        LABEL: "Node"
+        LABEL: "Circle",
+        MIRROR_MASTER_ANGLE: true
     };
-  // Backs
-    exports.node = {
+    Class.ankaxe_triangle = {
+        PARENT: ["genericTank"],
+        LABEL: "",
+        SHAPE: 3,
+        MIRROR_MASTER_ANGLE: true
+    };
+    Class.ankaxe_flipped_triangle = {
+        PARENT: ["genericTank"],
+        LABEL: "",
+        SHAPE: 3.5,
+        MIRROR_MASTER_ANGLE: true
+    };
+    Class.ankaxe_square = {
+        PARENT: ["genericTank"],
+        LABEL: "",
+        SHAPE: 4,
+        MIRROR_MASTER_ANGLE: true
+    };
+    Class.ankaxe_pentagon = {
+        PARENT: ["genericTank"],
+        LABEL: "",
+        SHAPE: 5,
+        MIRROR_MASTER_ANGLE: true
+    };
+    Class.ankaxe_flipped_pentagon = {
+        PARENT: ["genericTank"],
+        LABEL: "",
+        SHAPE: 5.5,
+        MIRROR_MASTER_ANGLE: true
+    };
+    Class.ankaxe_hexagon = {
+        PARENT: ["genericTank"],
+        LABEL: "",
+        SHAPE: 6,
+        MIRROR_MASTER_ANGLE: true
+    };
+    Class.ankaxe_heptagon = {
+        PARENT: ["genericTank"],
+        LABEL: "",
+        SHAPE: 7,
+        MIRROR_MASTER_ANGLE: true
+    };
+    Class.ankaxe_flipped_heptagon = {
+        PARENT: ["genericTank"],
+        LABEL: "",
+        SHAPE: 7.5,
+        MIRROR_MASTER_ANGLE: true
+    };
+  // Weapons
+    Class.ankaxe_node = {
         PARENT: ["genericTank"],
         LABEL: "Node"
     };
   // Bodies
-    exports.node = {
+    Class.ankaxe_base = {
         PARENT: ["genericTank"],
-        LABEL: "Node"
+        LABEL: "Base",
+        TURRETS: [
+          {
+            POSITION: [10, 0, 0, 0, 360, 1],
+            TYPE: ["ankaxe_circle"]
+          }
+        ],
     };
 };
