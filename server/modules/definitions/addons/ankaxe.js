3f1f584ee2394e88105477143a59569597431d76
richardgrechko <162537734+richardgrechko@users.noreply.github.com>
2024-03-19 08:15:22 +0500

    Update ankaxe.js

--- server/modules/definitions/addons/ankaxe.js
+++ server/modules/definitions/addons/ankaxe.js
@@ -1,1 +1,20 @@
-
+const { combineStats, addAura, makeAuto } = require('../facilitators.js');
+const { gunCalcNames, smshskl, base } = require('../constants.js');
+const g = require('../gunvals.js');
+module.exports = {
+  // Weapons
+    exports.node = {
+        PARENT: ["genericTank"],
+        LABEL: "Node"
+    };
+  // Backs
+    exports.node = {
+        PARENT: ["genericTank"],
+        LABEL: "Node"
+    };
+  // Bodies
+    exports.node = {
+        PARENT: ["genericTank"],
+        LABEL: "Node"
+    };
+};
