f783aa4e9e022f64d51b3f96a66171a8e043c5e2
richardgrechko <162537734+richardgrechko@users.noreply.github.com>
2024-04-24 10:19:39 +0500

    Update config.js

--- server/config.js
+++ server/config.js
@@ -3,7 +3,7 @@ module.exports = {
 
     // Game server domain.
     // If 'localhost:NUMBER', the port must equal the port setting.
-    host: "ankaxe.github.io",
+    host: "richardgrechko.github.io/ankaxe",
 
     // Which port to run the web server on.
     port: 26301,
